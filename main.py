c37a86fbe7d6ccab64eefcd2c091903d30cc9bb7
masterWind007 <vskozlov79@gmail.com.com>
2023-03-24 18:38:10 +0300

    Добавил команду /ocr

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def exec(message):
         if message.text=='/menu'    : chat.main_menu(message) # Вызов главного меню
         elif message.text=='/swchat'  : chat.swchat(message)   # Перенаравление в чат
         elif message.text=='/url'     : chat.url(message)      # Выдача ссылки
-        elif message.text=='/ocr'     : pass
+        elif message.text=='/ocr'     : chat.ocr_mode_on(message)
         else  : chat.say(message)      # Анализ и обработака текстовых сообщений
     else: 
         chat.bot.send_message(message.chat.id, chat.rand_ansv(chat.chat_logon['acc_no']))         
