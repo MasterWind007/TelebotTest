695ccc5e54b8e6996b14185eaed9f4d9d5dad43f
Valeriy <vaskozlov79@gmail.com>
2023-03-25 00:04:05 +0300

    11

--- ocrmodule.py
+++ ocrmodule.py
@@ -3,11 +3,15 @@ import cv2
 
 class OcrClass:
     def __init__(self, file_path):
-        self.teceract_exe_path = ''
-        pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+        self.teceract_exe_path = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+        pytesseract.pytesseract.tesseract_cmd = self.teceract_exe_path
         self.img = cv2.imread(file_path)
         self.img_copy = None
 
+    def  set_tesseract_path(self, path):
+        self.teceract_exe_path = path
+        pytesseract.pytesseract.tesseract_cmd = self.teceract_exe_path
+        
     def img_from_file(self, file_path):
         self.img = cv2.imread(file_path)
 
