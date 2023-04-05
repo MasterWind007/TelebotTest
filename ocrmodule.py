a7363cadf7a7e3c2896183804e657066ba4cc7b9
masterWind007 <vskozlov79@gmail.com.com>
2023-04-05 14:26:08 +0300

    Изменил модуль OCR

--- ocrmodule.py
+++ ocrmodule.py
@@ -3,10 +3,12 @@ from pytesseract import pytesseract as Pt
 import cv2
 
 class OcrClass:
-    def __init__(self, file_path):
-        self.teceract_exe_path = file_path
+    def __init__(self,  exe_path):
+        '''
+        exe_path - Путь к исполнимому модулю Teceract.exe
+        '''
+        self.teceract_exe_path = exe_path
         Pt.tesseract_cmd = self.teceract_exe_path
-        self.img = cv2.imread(file_path)
         self.img_copy = None
         self.t_conf = u"--psm 12"
         """
@@ -30,20 +32,38 @@ class OcrClass:
         """
 
     def  set_tesseract_path(self, path):
+        '''
+        Устанавливает путь к исполнимому модулю
+        path - Путь к исполнимому модулю Teceract.exe
+        '''
         self.teceract_exe_path = path
         Pt.tesseract_cmd = self.teceract_exe_path
         
-    def img_from_file(self, file_path):
-        self.img = cv2.imread(file_path)
-
-    def image_to_string(self):
-        return Pt.image_to_string(self.img, lang ='rus+eng', config = self.t_conf)
+    def image_to_string(self, file_path):
+        '''
+        Возвращает текст распознаный на картинке
+        file_path - Путь к файлу с картинкой
+        '''
+        img = cv2.imread(file_path)
+        return Pt.image_to_string(img, lang ='rus+eng', config = self.t_conf)
  
-    def image_to_data(self):
-        return Pt.image_to_data(self.img, lang='rus+eng', output_type=Pt.Output.DICT)
+    def image_to_data(self, file_path):
+        '''
+        Распознает текст и 
+        возвращает структуруру данных :
+            BYTES = 'bytes'
+            DATAFRAME = 'data.frame'
+            DICT = 'dict'
+            STRING = 'string'
+        
+        file_path - Путь к файлу с картинкой
+        '''
+        img = cv2.imread(file_path)
+        return Pt.image_to_data(img, lang='rus+eng', output_type=Pt.Output.DICT)
     
-    def emphasize_from_img(self, tg_word = ''):
-        img_copy = self.img.copy()
+    def emphasize_from_img(self,  file_path, tg_word = ''):
+        img = cv2.imread(file_path)
+        img_copy = img.copy()
         data = Pt.image_to_data(self.img, output_type=Pt.Output.DICT)
         word_rects = [i for i, word in enumerate(data["text"]) if word.lower().startswith(tg_word)]
         for rects in word_rects:
