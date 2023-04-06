8138801f03eca90c630ec909870213973b867302
masterWind007 <vskozlov79@gmail.com.com>
2023-04-06 18:29:37 +0300

    1

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,33 +1,47 @@
 
-lst = {'a': 8,
-       'b': 4,
-       'c': 13,
-       'd': 10,
-       'e': 170,
-       'f': 30}
+# lst = {'a': 8,
+#        'b': 4,
+#        'c': 13,
+#        'd': 10,
+#        'e': 170,
+#        'f': 30}
 
-def sort (list):
-    prev_key = ''
-    max_val = 0
-    max_key = ''
-    for i in list:
-        if list.get(i) > max_val: 
-            prev_key = max_key  
-            max_val = list.get(i)
-            max_key = i
-    return((prev_key, max_key))
+# def sort (list):
+#     prev_key = ''
+#     max_val = 0
+#     max_key = ''
+#     for i in list:
+#         if list.get(i) > max_val: 
+#             prev_key = max_key  
+#             max_val = list.get(i)
+#             max_key = i
+#     return((prev_key, max_key))
 
-def sort_next(list):
-    prev_key = []
-    max_val = 0
-    max_key = ''
-    for i in list:
-        if list.get(i) >= max_val: 
-            prev_key.append(max_key)  
-            max_val = list.get(i)
-            max_key = i
-    return((prev_key[1:], max_key))
+# def sort_next(list):
+#     prev_key = []
+#     max_val = 0
+#     max_key = ''
+#     for i in list:
+#         if list.get(i) >= max_val: 
+#             prev_key.append(max_key)  
+#             max_val = list.get(i)
+#             max_key = i
+#     return((prev_key[1:], max_key))
        
 
-print (sort (lst))
-# print (sort_next (lst))
+# print (sort (lst))
+# # print (sort_next (lst))
+
+
+def ask(func):
+    def inner(*args):
+        val = func(*args)
+        text = 'Сумма чисел равна '+ str(val)
+        return text
+    return inner
+
+@ask
+def summ(a,b):
+    return a+b
+
+print(summ(2,2))
