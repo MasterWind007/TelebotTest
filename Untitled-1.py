b83523515494619135cacf59e52dba10ba2c8932
masterWind007 <vskozlov79@gmail.com.com>
2023-04-07 11:32:36 +0300

    Google cloud vision

--- Untitled-1.py
+++ Untitled-1.py
@@ -33,15 +33,35 @@
 # # print (sort_next (lst))
 
 
-def ask(func):
-    def inner(*args):
-        val = func(*args)
-        text = 'Сумма чисел равна '+ str(val)
-        return text
-    return inner
+# def ask(func):
+#     def inner(*args):
+#         val = func(*args)
+#         text = 'Сумма чисел равна '+ str(val)
+#         return text
+#     return inner
 
-@ask
-def summ(a,b):
-    return a+b
+# @ask
+# def summ(a,b):
+#     return a+b
 
-print(summ(2,2))
+# print(summ(2,2))
+import os
+from google.cloud import vision
+
+# Укажите свой API-ключ
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'Comon/Res/ocr-projets002255-f3f7fba2d684.json'
+
+# Инициализируем объект класса VisionClient
+vision_client = vision.ImageAnnotatorClient()
+
+# открываем изображение
+with open(r'Comon\\Tmp\\ocrimg.jpg', 'rb') as image_file:
+    image = vision.Image(content=image_file.read())
+
+# получаем результат
+response = vision_client.text_detection(image=image)
+texts = response.text_annotations
+
+# выводим текст из изображения
+for text in texts:
+    print(text.description)
