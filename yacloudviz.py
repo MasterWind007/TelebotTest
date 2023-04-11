d3aa36680dc0c638e0cb5faf933f0fa3b5bd29a5
masterWind007 <vskozlov79@gmail.com.com>
2023-04-11 14:43:08 +0300

    Обернул в класс

--- yacloudviz.py
+++ yacloudviz.py
@@ -2,103 +2,49 @@ import base64
 import json
 import os
 import subprocess
-from abc import ABCMeta, abstractmethod, abstractproperty
+from pathlib import Path
 
+# folderid = "b1g9m7ogbnpo0afrth9g"
 
-class OCRif():
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def init():
-        ...
-
-    @abstractmethod
-    def read_from_file():
-        ...
-
-    @abstractmethod
-    def image_to_data():
-        ...
-
-    @abstractmethod
-    def image_to_string():
-        ...
-
-class YandexOCRinit():
-    def __init(self, OAuth=''):
-        self.o_auth= OAuth
-        self.out = { #распознавание текста
-                        "folderId": folderid ,
-                        "analyze_specs": [{
-                           "content": outfile,
-                           "features": [{
-                                "type": "TEXT_DETECTION",
-                                "text_detection_config": {
-                                    "language_codes": ["*"]
-                                }
-                            }]
-                        }]
+class YandexOCR():
+    def __init__(self, OAuth='', folder_id=''):
+        self.outfile=''
+        self.o_auth = OAuth
+        self.folder_id = folder_id
+ 
+    def get_aim(self, OAuth=''):
+        cloud_id = {"yandexPassportOauthToken": OAuth}
+        get_token = ''.join(("curl -X POST -H 'Content-Type: application/json' -d \"", str(cloud_id), "\" ",\
+                     "https://iam.api.cloud.yandex.net/iam/v1/tokens")) 
+        IAM_TOKEN = subprocess.check_output(get_token,  shell=True)
+        return str(IAM_TOKEN[16:-49])[2:-5]
+    
+    def encode_file(file):
+        with open(file, 'rb') as f:
+            file_content = f.read()
+        return base64.b64encode(file_content).decode('utf-8')
+    
+    def image_to_json(self, file_img =Path('Comon','Tmp','ocrimg.jpg')):
+        outfile = self.encode_file(file_img)
+        out = {     #распознавание текста
+            "folderId": self.folder_id,
+            "analyze_specs": [{
+                "content": outfile,
+                "features": [{
+                    "type": "TEXT_DETECTION",
+                    "text_detection_config": {
+                        "language_codes": ["*"]
                     }
-        
-
-
-
-def encode_file(file):
-    with open(file, 'rb') as f:
-        file_content = f.read()
-    return base64.b64encode(file_content).decode('utf-8')
-folderid = "b1g9m7ogbnpo0afrth9g"
-outfile = encode_file('Comon\Tmp\ocrimg.jpg') #my image file 
-
-out = {     #распознавание текста
-    "folderId": folderid ,
-    "analyze_specs": [{
-        "content": outfile,
-        "features": [{
-            "type": "TEXT_DETECTION",
-            "text_detection_config": {
-                "language_codes": ["*"]
-            }
-        }]
-    }]
-}
-
-# out = {  #  поиск морды лица
-#     "folderId": folderid,
-#     "analyze_specs": [{
-#         "content": outfile,
-#         "features": [{
-#             "type": "CLASSIFICATION",
-#             "classificationConfig": {
-#                 "model": "quality"
-#             }
-# },
-#         {
-#             "type": "CLASSIFICATION",
-#             "classificationConfig": {
-#                 "model": "moderation"
-#             }
-#         },
-#             {
-#             "type": "FACE_DETECTION"
-#         }]
-#     }]
-# }
+                }]
+            }]
+        }
+        with open('body.json', 'w') as f:
+            json.dump(out, f)
+        IAM_TOKEN = self.get_aim(self.o_auth)
+        crl = 'curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer '+IAM_TOKEN+'" -d @body.json https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze > output.json'
+        export_req = ''.join(('export IAM_TOKEN=', IAM_TOKEN ))
+        to_curl = ''.join(crl)
+        os.system(export_req)
+        os.system(to_curl)
     
-#Готовим запрос
-with open('body.json', 'w') as f:
-    json.dump(out, f)
-    
-cloud_id = {"yandexPassportOauthToken": "y0_AgAAAAA2GPPIAATuwQAAAADeYXG_MunkevKMS_GIRQsbKNqepm-n3wg"}
-get_token = ''.join(("curl -X POST -H 'Content-Type: application/json' -d \"", str(cloud_id), "\" ",\
-                     "https://iam.api.cloud.yandex.net/iam/v1/tokens"))
 
-IAM_TOKEN = subprocess.check_output(get_token,  shell=True)
-IAM_TOKEN = str(IAM_TOKEN[16:-49])[2:-5] 
-crl = 'curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer '+IAM_TOKEN+'" -d @body.json https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze > output.json'
-export_req = ''.join(('export IAM_TOKEN=', IAM_TOKEN ))
-to_curl = ''.join(crl)
-
-# Получаем ответ
-os.system(export_req)
-os.system(to_curl)
